a58d51507291154ee509b19afde8dfbc9b142372
Ubirajara Neves <ubirajara.neves@usp.br>
2024-02-23 17:53:06 -0300

    Remove accents from contact names

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { DividerComponent } from './components/divider/divider.component';
 import { ContactComponent } from './components/contact/contact.component';
 import contactsList from './agenda.json';
+import { FormsModule } from '@angular/forms';
 
 interface Contact {
   id: number;
@@ -25,15 +26,35 @@ interface Contact {
     HeaderComponent,
     DividerComponent,
     ContactComponent,
+    FormsModule,
   ],
 })
 export class AppComponent {
   alphabet: string = 'abcdefghijklmnopqrstuvwxyz';
   contacts: Contact[] = contactsList;
+  filterByText: string = '';
+
+  private removeAccents(text: string): string {
+    return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
+
+  filterContactsByText(): Contact[] {
+    if (!this.filterByText) return this.contacts;
+
+    const filteredContacts = this.contacts.filter((contact) =>
+      this.removeAccents(contact.name)
+        .toLowerCase()
+        .includes(this.removeAccents(this.filterByText).toLowerCase())
+    );
+
+    return filteredContacts;
+  }
 
   filterContactsByInitial(letter: string): Contact[] {
-    const filteredContacts = this.contacts.filter((contact) =>
-      contact.name.toLowerCase().startsWith(letter)
+    const filteredContacts = this.filterContactsByText().filter((contact) =>
+      this.removeAccents(contact.name)
+        .toLowerCase()
+        .startsWith(this.removeAccents(letter))
     );
 
     return filteredContacts;
