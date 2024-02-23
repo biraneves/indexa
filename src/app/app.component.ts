1c409ac3a20c75418c4cb35eb18307e215fa3250
Ubirajara Neves <ubirajara.neves@usp.br>
2024-02-23 16:12:29 -0300

    Header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './components/container/container.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [CommonModule, RouterOutlet, ContainerComponent],
+  imports: [CommonModule, RouterOutlet, ContainerComponent, HeaderComponent],
 })
 export class AppComponent {
   title = 'indexa';
