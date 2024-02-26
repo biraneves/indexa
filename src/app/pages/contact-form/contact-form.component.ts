f7009b3e6cf8f754d0c02329043a5f55deb7b788
Ubirajara Neves <ubirajara.neves@usp.br>
2024-02-26 08:50:00 -0300

    Form validation

--- src/app/pages/contact-form/contact-form.component.ts
+++ src/app/pages/contact-form/contact-form.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../components/container/container.component';
 import { DividerComponent } from '../../components/divider/divider.component';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'bn-contact-form',
   standalone: true,
-  imports: [ContainerComponent, DividerComponent],
+  imports: [
+    ContainerComponent,
+    DividerComponent,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css',
 })
-export class ContactFormComponent {}
+export class ContactFormComponent {
+  contactForm: FormGroup;
+
+  constructor() {
+    this.contactForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      phone: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      birthday: new FormControl(''),
+      social: new FormControl(''),
+      obs: new FormControl(''),
+    });
+  }
+
+  saveContact() {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+    }
+  }
+}
